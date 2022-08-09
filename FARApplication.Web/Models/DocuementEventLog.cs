e6d5199ab8cbbcf408d414965395306524993e8e
Nilanjan Saha <nilanjan.saha@henkel.com>
2022-08-09 13:47:04 +0200

    Refining the Model

---
+++ FARApplication.Web/Models/DocuementEventLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FARApplication.Web.Models
+{
+    public class DocuementEventLog
+    {
+        public int FARId { get; set; }
+        public int UserId { get; set; }
+        public string  Message { get; set; }
+        public DateTime EventDate { get; set; }
+    }
+}
---
+++ FARApplication.Web/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FARApplication.Web.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string  FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string  LastName { get; set; }
+        public string  EmailId { get; set; }
+        public string  Password { get; set; }
+
+
+    }
+}
