05d56c1e9bfbdaaab5220e7c777ae5dc63978f50
Nilanjan Saha <nilanjan.saha@henkel.com>
2022-08-09 16:26:38 +0200

    Revert "Refining the Model"
    This reverts commit e6d5199ab8cbbcf408d414965395306524993e8e.

--- FARApplication.Web/Models/FAR.cs
+++ FARApplication.Web/Models/FAR.cs
@@ -8,22 +8,15 @@ namespace FARApplication.Web.Models
     public class FAR
     {
         public int Id { get; set; }
-        // Document Information
         public string RequestId { get; set; }
-        public int Status { get; set; } // 1 = Draft , 2 = Send for Approval, 3 = First Level approved, 4 = approved, 5=Rejected
-        public int UserId { get; set; } // Created By. No user Input, User Id should be saved based on the login email
-        public DateTime CreatationDate { get; set; } // Sytem Date Time. No Input. 
+        public int Status { get; set; }
+        public string CreatedBy { get; set; }
 
-        // Request Information
+        public DateTime CreatedOn { get; set; }
 
         public string Summary { get; set; }
+
         public string Details { get; set; }
-
-        // Approver Information
-        public List<Approver> Approvers { get; set; }
-
-        //Document Event Log
-        public List<DocuementEventLog> DocuementEventLogs { get; set; }
 
     }
 }
