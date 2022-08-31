27dc39922fbbf724654a296e679b617d6e9eaac5
Nilanjan Saha <nilanjan.saha@henkel.com>
2022-08-31 12:57:25 +0200

    vaibhab

--- FARApplication.Web/Models/FAR.cs
+++ FARApplication.Web/Models/FAR.cs
@@ -9,7 +9,7 @@ namespace FARApplication.Web.Models
     /// This model class maintains the state of a FAR.
     /// </summary>
     public class FAR
-    {
+    {    {
         public FAR()
         {
             FAREventLogs = new List<FAREventLog>();
@@ -21,18 +21,17 @@ namespace FARApplication.Web.Models
         public int Status { get; set; }
         public DocumentStatus LifeCycleStatus { get; set; }
         public int UserId { get; set; }
-        public string  CreatedBy { get; set; }
+        public string CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
-
         //Request Information
-        [Required(ErrorMessage ="Please enter summary of the FAR.")]
+        [Required(ErrorMessage = "Please enter summary of the FAR.")]
         public string Summary { get; set; }
         [Required(ErrorMessage = "Please enter details of the FAR.")]
         public string Details { get; set; }
-        public string  Filename { get; set; }
         public List<FAREventLog> FAREventLogs { get; set; }
-        public  User User { get; set; }
-      
+        public User User { get; set; }
         public List<FARApprover> Approverdetails { get; set; }
+        [MaxLength(2000, ErrorMessage = "The Filename must be less that 300 characters")]
+        public string Filename { get; set; }
     }
 }
